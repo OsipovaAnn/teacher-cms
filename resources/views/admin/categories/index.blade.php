da3c764783a5cce0166024ce4f544f1c2346a0d8
Osipova Anna <annaosipova21@yandex.ru>
2018-02-16 13:23:01 +0300

    Create category.index page'

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,43 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+<div class="container">
+
+  @component('admin.components.breadcrumb')
+    @slot('title') Список категорий @endslot
+    @slot('parent') Главная @endslot
+    @slot('active') Категории @endslot
+  @endcomponent
+
+  <hr>
+
+    <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right">
+      <i class="fa fa-plus-square-o"></i> Создать категорию
+    </a>
+    <table class="table table-striped">
+        <thead>
+          <th>Наименование</th>
+          <th>Публикация</th>
+          <th class="text-right">Действие</th>
+        </thead>
+        <tbody>
+          <tfoot>
+      <tr>
+          <td colspan="3">
+              <ul class="pagination pull-right">
+                  {{$categories->links()}}
+              </ul>
+          </td>
+      </tr>
+  </tfoot>
+
+      </table>
+
+//список категорий
+
+</div>
+
+//список категорий
+
+@endsection
